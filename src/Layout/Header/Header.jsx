0a6dd9abaa8a420996b5bd8686a772fdc334181f
najmul19977 <najmulkabir19977@gmail.com>
2023-06-27 23:05:33 +0600

    onather part commit

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <h1 className="text-5xl font-bold">About Me!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-primary">Download Resume</button>
                     </div>
                 </div>
             </div>
